e1ea05eaa1dd976af9dd860c67243152d3c9f2e9
eusebiops14 <64171953+eusebiops14@users.noreply.github.com>
2021-03-16 08:19:25 -0300

    dia 9 pt1

---
+++ 9_NODE/130_ProjetoFuncionarios/funcionarios.js
@@ -0,0 +1,30 @@
+//link : http://files.cod3r.com.br/curso-js/funcionarios.json
+//instalar axios pelo terminal: npm install --save axios
+
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
+const axios = require('axios');
+
+const SelecionaChineses = p => p.pais === 'China';
+const SelecionaMulheres = p =>p.genero === 'F';
+const SelecionaMenorSalario = (func,funcAtual) => {
+    return func.salario < funcAtual.salario ? func : funcAtual;
+}
+
+axios.get(url).then(response => {
+    const funcionarios = response.data;
+    console.log(typeof funcionarios); //retorna funcionarios como um objeto
+    
+    //aula 131: resolução desafio
+    const func = funcionarios
+        .filter(SelecionaChineses)
+        .filter(SelecionaMulheres)
+        .reduce(SelecionaMenorSalario);
+console.log(func);
+    
+    
+    
+    
+    //console.log(typeof func);
+
+
+})
