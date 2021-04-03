27d2fbd30167c4fe45fab6d0472acc83c6175b50
eusebiops14 <64171953+eusebiops14@users.noreply.github.com>
2021-04-03 12:27:18 -0300

    teste

--- 14_INTEGRANDO_HTML_CSS_JS/dom/js/flappy.js
+++ 14_INTEGRANDO_HTML_CSS_JS/dom/js/flappy.js
@@ -10,18 +10,21 @@ function Barreira(reversa=false) {
     const borda = novoElemento('div', 'borda');
     const corpo = novoElemento('div','corpo');
     this.elemento.appendChild(reversa ? corpo : borda);
-    this.elemento.appendChild(reversa ? corpo : borda);
+    this.elemento.appendChild(reversa ? borda : corpo);
     
     this.setAltura = altura => corpo.style.height = `${altura}px`
     
 }
 
-/*
-const b = new Barreira(true);
-b.setAltura(200);
-document.querySelector('[wm-flappy]').appendChild(b.elemento);
-*/
 
+const b = new Barreira(true);
+b.setAltura(400);
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+
+
+
+/*
 function ParDeBarreiras(altura,abertura,x) {
     this.elemento = novoElemento('div','par-de-barreiras');
 
@@ -49,5 +52,5 @@ function ParDeBarreiras(altura,abertura,x) {
 
 const b = new ParDeBarreiras(700,200,400);
 document.querySelector('[wm-flappy').appendChild(b.elemento);
-
+*/
 
