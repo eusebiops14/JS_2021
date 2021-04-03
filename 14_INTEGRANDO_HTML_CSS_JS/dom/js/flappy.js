f18f03b12363e0e4c693798e94f9894b26713e7a
eusebiops14 <64171953+eusebiops14@users.noreply.github.com>
2021-04-03 11:03:57 -0300

    dia 21

--- 14_INTEGRANDO_HTML_CSS_JS/dom/js/flappy.js
+++ 14_INTEGRANDO_HTML_CSS_JS/dom/js/flappy.js
@@ -15,3 +15,67 @@ function Barreira(reversa=false) {
     this.setAltura = altura => corpo.style.height = `${altura}px`
     
 }
+
+function ParDeBarreiras(altura,abertura,x) {
+    this.elemento = novoElemento('div','par-de-barreiras');
+    this.superior = new Barreira(true);
+    this.inferior = new Barreira(false);
+
+    this.elemento.appendChild(this.superior.elemento);
+    this.elemento.appendChild(this.inferior.elemento);
+
+    this.sortearAbertura = () => {
+        const alturaSupeior = Math.random() * (altura - abertura);
+        const alturaInferior = altura - abertura - alturaSupeior;
+
+        this.superior.setAltura(alturaSupeior);
+        this.inferior.setAltura(alturaInferior);
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0]);
+    this.setX = x => this.elemento.style.left = `${x}px`;
+    this.getLargura = () => this.elemento.clientWidth;
+
+    this.sortearAbertura();
+    this.setX(x);
+}
+
+//const b = new ParDeBarreiras (700,200,800);
+//document.querySelector('[wm-flappy]').appendChild(b.elemento);
+
+function Barreiras(altura,largura,abertura,espaco, notificarPonto) {
+    this.pares = [
+        new ParDeBarreiras(altura,abertura,largura),
+        new ParDeBarreiras(altura,abertura,largura + espaco),
+        new ParDeBarreiras(altura,abertura,largura + espaco *2),
+        new ParDeBarreiras(altura,abertura,largura +espaco * 3)
+    ]
+
+    const deslocamento = 3
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX()-deslocamento);
+
+            //quando elemento sair da area do jogo
+            if (par.getX < -par.getLargura()) {
+                par.setX(par.getX() + espaco * this.pares.length);
+                par.sortearAbertura();
+
+            }
+
+            const meio = largura/2;
+            const cruzouMeio = par.getX() + deslocamento >= meio 
+                && par.getLargura(x) < meio;
+            if (cruzouMeio) {
+                notificarPonto()
+            }
+        })
+    }
+}
+
+const barreiras = new Barreiras(700,1200,200,400);
+const areaDoJogo = document.querySelector(['wm-flappy']);
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento));
+setInterval(() => {
+    barreiras.animar()
+},20);
