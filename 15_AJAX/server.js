760f382f9bf19674222335657e30fb4c25b8913a
eusebiops14 <64171953+eusebiops14@users.noreply.github.com>
2021-04-04 11:37:58 -0300

    Ajax cap 15

---
+++ 15_AJAX/server.js
@@ -0,0 +1,29 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+app.use(express.static('.')); //fornece arquivos estáticos
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req,file,callback) {
+        callback(null,'./upload')
+    },
+    filename: function(req,file,callback) {
+        callback(null,`${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo');
+app.post('/upload', (req,res) => {
+    upload(req,res,err => {
+        if(err) {
+            return res.end('ERRO');
+        }
+        res.end('Concluído com sucesso')
+    })
+})
+//app.get('/teste', (req,res) => res.send('ok'));
+app.listen(8080, () => console.log('Executando...'));
